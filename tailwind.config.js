519312e45e123800dd463f628cc62a4e74203634
Prem-Ray <premanshuray981@gmail.com>
2024-09-15 12:12:35 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      screens:{
+        'xsm':'312px',
+        'lgr':'1084px',
+        'xlg':'1130px' ,
+        'xsm':'542px' ,
+        '3xl':'1905px',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
